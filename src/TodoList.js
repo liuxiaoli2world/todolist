b2fc24a797479c8c7f5e64e03c1dbe0bfa7aa64b
liuxiaoli2world <liuxiaoli2world@gmail.com>
2019-03-02 17:51:26 +0800

    优化组件 1、构造函数中绑定 this 2、用解构赋值 3、this.setState方法采用异步调用 4、生成子组件抽取成方法

--- src/TodoList.js
+++ src/TodoList.js
@@ -3,7 +3,7 @@
  * @LastEditors: liuxiaoli
  * @Description: 代办组件
  * @Date: 2019-03-01 15:56:57
- * @LastEditTime: 2019-03-02 16:48:02
+ * @LastEditTime: 2019-03-02 17:50:03
  */
 import React, { Component, Fragment } from 'react';
 import TodoItem from './TodoItem';
@@ -23,59 +23,70 @@ class TodoList extends Component {
 
   /**
    * @description: 输入框内容改变事件处理
-   * @param {event} 
-   * @return: 
+   * @param {event}
+   * @return:
    */
   onChange(event) {
-    this.setState({ inputValue: event.target.value });
+    const value = event.target.value;
+    this.setState(() => ({
+      inputValue: value
+    }));
   }
 
   /**
    * @description: 添加按钮点击事件处理
-   * @param {event} 
-   * @return: 
+   * @param {event}
+   * @return:
    */
   handleBtnClick(event) {
-    this.setState({
+    this.setState(preState => ({
       inputValue: '',
-      list: [this.state.inputValue, ...this.state.list]
-    });
+      list: [...preState.list, preState.inputValue]
+    }));
   }
 
   /**
    * @description: 列表项目点击事件处理
    * @param {number} 点击项目的索引
-   * @return: 
+   * @return:
    */
   handleItemClick(index) {
-    const list = [...this.state.list];
-    list.splice(index, 1);
-    this.setState({
-      list
+    this.setState(preState => {
+      const list = [...preState.list];
+      list.splice(index, 1);
+      return { list };
+    });
+  }
+  
+  /**
+   * @description: 生成列表
+   * @return: 
+   */
+  geneListItems() {
+    return this.state.list.map((item, index) => {
+      return (
+        <TodoItem
+          key={index}
+          content={item}
+          index={index}
+          handleItemClick={this.handleItemClick}
+        />
+      );
     });
   }
 
   render() {
+    const { inputValue } = this.state;
     return (
       <Fragment>
         <input
           type="text"
           className="input"
-          value={this.state.inputValue}
+          value={inputValue}
           onChange={this.onChange}
         />
         <button onClick={this.handleBtnClick}>添加</button>
-        <ul>
-          {this.state.list.map((item, index) => {
-            return (
-              <TodoItem
-                content={item}
-                index={index}
-                handleItemClick={this.handleItemClick}
-              />
-            );
-          })}
-        </ul>
+        <ul>{this.geneListItems()}</ul>
       </Fragment>
     );
   }
